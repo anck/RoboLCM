5d54a75021c531e12e189538658229a65b68ba55
anck <myscoob@gmail.com>
2014-06-01 21:14:57 -0400

    Shoot and health

---
+++ Assets/scripts/ShootingControl.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootingControl : MonoBehaviour 
+{
+	// 1 - Designer variables
+	
+	/// <summary>
+	/// Damage inflicted
+	/// </summary>
+	public int damage = 1;
+	
+	/// <summary>
+	/// Projectile damage player or enemies?
+	/// </summary>
+	public bool isEnemyShot = false;
+	
+	void Start()
+	{
+		// 2 - Limited time to live to avoid any leak
+		Destroy(gameObject, 2); // 20sec
+	}
+
+	/// <summary>
+	/// Object speed
+	/// </summary>
+	public Vector2 speed = new Vector2(1, 1);
+	
+	/// <summary>
+	/// Moving direction
+	/// </summary>
+	public Vector2 direction = new Vector2(1, 0);
+	
+	private Vector2 movement;
+	
+	void Update()
+	{
+		// 2 - Movement
+		movement = new Vector2(
+			speed.x * direction.x,
+			speed.y * direction.y);
+	}
+	
+	void FixedUpdate()
+	{
+		// Apply movement to the rigidbody
+		rigidbody2D.velocity = movement;
+	}
+}
